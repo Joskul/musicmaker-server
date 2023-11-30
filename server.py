b86bd51570b512e1246e03c87c3d4034a17c87d9
Schpudt <hikaru.kp@gmail.com>
2023-11-30 21:04:24 +0700

    Restructured + switch to FastAPI

--- server.py
+++ server.py
@@ -1,101 +1,122 @@
-import uuid
 import os
-from flask import Flask, request, jsonify, send_file, session
-from flask_cors import CORS
+import uuid
+from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.responses import JSONResponse, FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 from pytube import YouTube
 
-app = Flask(__name__)
-CORS(app)
+app = FastAPI()
+origins = ["*"]
 
-UPLOAD_FOLDER = 'uploads'
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+PROCESS_FOLDER = 'processes'
+AUDIO_FILE_NAME = 'audio.mp3'
 ALLOWED_EXTENSIONS = {'mp3', 'wav', 'ogg', 'flac'}
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-# Store user-specific file information
+# Store user-specific process information
 user_files = {}
 
 
+def save_process(user_id: str, process_id: str, process_name: str):
+    '''Save user process and return process ID'''
+    # Update user-specific file information
+    user_files[user_id] = {'process_id': process_id,
+                           'process_name': process_name}
+
+    # Return the file path as a response
+    return JSONResponse(content={'process_id': process_id, 'process_name': process_name})
+
+
 def allowed_file(filename):
+    '''Check for allowed file type'''
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return jsonify({'error': 'No file part'})
-
-    file = request.files['file']
+@app.post("/upload")
+async def upload_file(file: UploadFile, user_id: str):
+    '''Receive files'''
+    if not file:
+        return JSONResponse(content={'error': 'No file part'}, status_code=400)
 
     if file.filename == '':
-        return jsonify({'error': 'No selected file'})
+        return JSONResponse(content={'error': 'No selected file'}, status_code=400)
 
-    if file and allowed_file(file.filename):
+    if allowed_file(file.filename):
         # Generate a unique filename
-        unique_filename = str(uuid.uuid4()) + '.' + \
-            file.filename.rsplit('.', 1)[1].lower()
-        user_id = request.args.get('user_id')
+        unique_id = str(uuid.uuid4())
 
-        # Save the file to the upload folder
+        # Save the file to the process folder
         file_path = os.path.join(
-            app.root_path, app.config['UPLOAD_FOLDER'], unique_filename)
-        file.save(file_path)
-
-        # Update user-specific file information
-        user_files[user_id] = {'file_path': file_path,
-                               'file_name': unique_filename}
+            PROCESS_FOLDER, unique_id)
+        if not os.path.exists(file_path):
+            os.makedirs(file_path)
+        with open(os.path.join(file_path, AUDIO_FILE_NAME), "wb") as f:
+            f.write(file.file.read())
 
         # Return the unique identifier associated with the uploaded file
-        return jsonify({'file_id': unique_filename})
+        return save_process(user_id, unique_id, file.filename)
 
-    return jsonify({'error': 'Invalid file type'})
+    return JSONResponse(content={'error': 'Invalid file type'}, status_code=400)
 
 
-@app.route('/process_video', methods=['POST'])
-def download_audio():
-    data = request.get_json()
-
-    if 'videoId' not in data or 'userId' not in data:
-        return jsonify({'error': 'Invalid request'})
+@app.post("/process_url")
+async def download_audio(data: dict):
+    '''Get audio URL'''
+    if 'videoId' not in data or 'userId' not in data or 'format' not in data:
+        return JSONResponse(content={'error': 'Invalid request'}, status_code=400)
 
     video_id = data['videoId']
     user_id = data['userId']
+    request_format = data['format']
 
-    try:
-        # Download the YouTube video audio
-        youtube_url = f'https://www.youtube.com/watch?v={video_id}'
-        yt = YouTube(youtube_url)
-        audio_stream = yt.streams.filter(only_audio=True).first()
-        # Generate a unique filename
-        unique_filename = str(uuid.uuid4()) + '.' + \
-            (video_id+user_id).rsplit('.', 1)[0].lower() + '.mp3'
+    if (request_format == 'youtube'):
+        try:
+            # Download the YouTube video audio
+            youtube_url = f'https://www.youtube.com/watch?v={video_id}'
+            yt = YouTube(youtube_url)
+            audio_stream = yt.streams.filter(only_audio=True).first()
+            # Generate a unique filename
+            unique_id = str(uuid.uuid4())
 
-        audio_path = os.path.join(
-            app.root_path, app.config['UPLOAD_FOLDER'], unique_filename)
-        audio_stream.download(
-            output_path=app.config['UPLOAD_FOLDER'], filename=unique_filename)
+            audio_path = os.path.join(
+                PROCESS_FOLDER, unique_id)
 
-        # Update user-specific file information
-        user_files[user_id] = {'file_path': audio_path,
-                               'file_name': unique_filename}
+            if not os.path.exists(audio_path):
+                os.makedirs(audio_path)
 
-        # Return the file path as a response
-        return jsonify({'file_id': unique_filename})
+            # Download the file to the process folder
+            audio_stream.download(output_path=audio_path,
+                                  filename=AUDIO_FILE_NAME)
 
-    except Exception as e:
-        return jsonify({'error': str(e)})
+            # Return the file path as a response
+            return save_process(user_id, unique_id, audio_stream.title)
+
+        except Exception as e:
+            return JSONResponse(content={'error': str(e)}, status_code=500)
 
 
-@app.route('/send_file/<string:userId>')
-def return_user_file(userId):
-    if userId in user_files:
-        file_info = user_files[userId]
-        return send_file(file_info['file_path'], as_attachment=True, download_name=file_info['file_name'])
+@app.get("/get_file/{user_id}")
+async def return_user_file(user_id: str):
+    '''Send the file requested'''
+    if user_id in user_files:
+        file_info = user_files[user_id]
+        return FileResponse(path=os.path.join(PROCESS_FOLDER, file_info['process_id'], AUDIO_FILE_NAME),
+                            filename=AUDIO_FILE_NAME, media_type='application/octet-stream')
     else:
-        return jsonify({'error': 'User has no uploaded files'})
+        return JSONResponse(content={'error': 'User has no uploaded files'}, status_code=404)
 
 
 if __name__ == '__main__':
-    if not os.path.exists(UPLOAD_FOLDER):
-        os.makedirs(UPLOAD_FOLDER)
-    app.run(debug=True)
+    if not os.path.exists(PROCESS_FOLDER):
+        os.makedirs(PROCESS_FOLDER)
+    import uvicorn
+
+    uvicorn.run("main:app", host="127.0.0.1", port=5000,
+                log_level="info", reload=True)
