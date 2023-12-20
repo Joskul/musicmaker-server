445b80c3736c8c1839ec3ec582938e5665e3e345
Joskul <hikaru.kp@gmail.com>
2023-12-20 23:20:43 +0700

    Unblock origins

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from starlette.middleware.cors import CORSMiddleware
 from pytube import YouTube
 
 app = FastAPI()
-origins = ["https://beat-ml.vercel.app","http://localhost",]
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
