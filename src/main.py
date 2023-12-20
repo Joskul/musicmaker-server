717b2bf68f8ff509c44546b493649c880eaf446e
Joskul <hikaru.kp@gmail.com>
2023-12-20 23:17:41 +0700

    Limit origins

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ from starlette.middleware.cors import CORSMiddleware
 from pytube import YouTube
 
 app = FastAPI()
-origins = ["*"]
+origins = ["https://beat-ml.vercel.app","http://localhost",]
 
 app.add_middleware(
     CORSMiddleware,
