26be92b650b7857f164d68b2f12fe0ab4b2c3993
Joskul <hikaru.kp@gmail.com>
2023-12-21 01:45:09 +0700

    Minor fix

--- src/main.py
+++ src/main.py
@@ -179,5 +179,5 @@ if __name__ == '__main__':
         os.makedirs(PROCESS_FOLDER)
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=5000,
+    uvicorn.run("main:app", host="0.0.0.0", port=5000,
                 log_level="info", reload=True)
