e2847d097e4a894c2db7a2dfab19139bf97a0a5f
Joskul <hikaru.kp@gmail.com>
2023-12-21 12:27:03 +0700

    Code structurize

--- src/main.py
+++ src/main.py
@@ -1,118 +1,119 @@
 import os
+import sys
+import shutil
 import uuid
 from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import JSONResponse, FileResponse
 from starlette.middleware.cors import CORSMiddleware
-from spleeter.separator import Separator
-import shutil
-import os
-from basic_pitch.inference import predict_and_save
 
+
+# Online audio
 from pytube import YouTube
 
-import sys
-import librosa
+# MIDI conversion
+from basic_pitch.inference import predict_and_save
 
-from sound_to_midi.monophonic import wave_to_midi
+# Track splitter
+from spleeter.separator import Separator
 
 # initial and config
 app = FastAPI()
-separator = Separator('spleeter:2stems')
+separator = Separator("spleeter:4stems", multiprocess=False)
 origins = ["*"]
-ALLOWED_AUDIO_EXTENSIONS = {'.mp3', '.wav', '.ogg'}
+ALLOWED_AUDIO_EXTENSIONS = {".mp3", ".wav", ".ogg"}
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"]
+    allow_headers=["*"],
 )
 
-PROCESS_FOLDER = 'processes'
-AUDIO_FILE_NAME = 'audio.mp3'
-MIDI_FILE_NAME = 'midi.mid'
-ALLOWED_EXTENSIONS = {'mp3', 'wav', 'ogg', 'flac'}
+PROCESS_FOLDER = "processes"
+AUDIO_FILE_NAME = "audio.mp3"
+MIDI_FILE_NAME = "audio_basic_pitch.mid"
+ALLOWED_EXTENSIONS = {"mp3", "wav", "ogg", "flac"}
 
 # Store user-specific process information
 user_files = {}
 
+
 def save_youtube_audio(user_id: str, video_id: str):
     try:
-            # Download the YouTube video audio
-            youtube_url = f'https://www.youtube.com/watch?v={video_id}'
-            yt = YouTube(youtube_url)
-            audio_stream = yt.streams.filter(only_audio=True).first()
-            # Generate a unique filename
-            unique_id = str(uuid.uuid4())
+        # Download the YouTube video audio
+        youtube_url = f"https://www.youtube.com/watch?v={video_id}"
+        yt = YouTube(youtube_url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        # Generate a unique filename
+        unique_id = str(uuid.uuid4())
 
-            audio_path = os.path.join(
-                PROCESS_FOLDER, unique_id)
+        audio_path = os.path.join(PROCESS_FOLDER, unique_id)
 
-            if not os.path.exists(audio_path):
-                os.makedirs(audio_path)
+        if not os.path.exists(audio_path):
+            os.makedirs(audio_path)
 
-            # Download the file to the process folder
-            audio_stream.download(output_path=audio_path,
-                                  filename=AUDIO_FILE_NAME)
+        # Download the file to the process folder
+        audio_stream.download(output_path=audio_path, filename=AUDIO_FILE_NAME)
 
-            # Return the file path as a response
-            return save_process(user_id, unique_id, audio_stream.title)
+        # Return the file path as a response
+        return save_process(user_id, unique_id, audio_stream.title)
     except Exception as e:
-        return JSONResponse(content={'error': str(e)}, status_code=500)
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
 
 def save_spotify_audio(user_id: str, track_url: str):
     try:
-            # TODO: Download the Spotify Audio
+        # TODO: Download the Spotify Audio
 
-            # Generate a unique filename
-            unique_id = str(uuid.uuid4())
+        # Generate a unique filename
+        unique_id = str(uuid.uuid4())
 
-            audio_path = os.path.join(
-                PROCESS_FOLDER, unique_id)
+        audio_path = os.path.join(PROCESS_FOLDER, unique_id)
 
-            if not os.path.exists(audio_path):
-                os.makedirs(audio_path)
+        if not os.path.exists(audio_path):
+            os.makedirs(audio_path)
 
-            # Download the file to the process folder
-            audio_stream.download(output_path=audio_path,
-                                  filename=AUDIO_FILE_NAME)
+        # Download the file to the process folder
+        audio_stream.download(output_path=audio_path, filename=AUDIO_FILE_NAME)
 
-            # Return the file path as a response
-            return save_process(user_id, unique_id, audio_stream.title)
+        # Return the file path as a response
+        return save_process(user_id, unique_id, audio_stream.title)
     except Exception as e:
-        return JSONResponse(content={'error': str(e)}, status_code=500)
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+
 
 def save_process(user_id: str, process_id: str, process_name: str):
-    '''Save user process and return process ID'''
+    """Save user process and return process ID"""
     # Update user-specific file information
-    user_files[user_id] = {'process_id': process_id,
-                           'process_name': process_name}
+    user_files[user_id] = {"process_id": process_id, "process_name": process_name}
 
     # Return the file path as a response
-    return JSONResponse(content={'process_id': process_id, 'process_name': process_name})
+    return JSONResponse(
+        content={"process_id": process_id, "process_name": process_name}
+    )
 
 
 def allowed_file(filename):
-    '''Check for allowed file type'''
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    """Check for allowed file type"""
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @app.post("/process-file")
 async def upload_file(file: UploadFile, user_id: str):
-    '''Receive files'''
+    """Receive files"""
     if not file:
-        return JSONResponse(content={'error': 'No file part'}, status_code=400)
+        return JSONResponse(content={"error": "No file part"}, status_code=400)
 
-    if file.filename == '':
-        return JSONResponse(content={'error': 'No selected file'}, status_code=400)
+    if file.filename == "":
+        return JSONResponse(content={"error": "No selected file"}, status_code=400)
 
     if allowed_file(file.filename):
         # Generate a unique filename
         unique_id = str(uuid.uuid4())
 
         # Save the file to the process folder
-        file_path = os.path.join(
-            PROCESS_FOLDER, unique_id)
+        file_path = os.path.join(PROCESS_FOLDER, unique_id)
         if not os.path.exists(file_path):
             os.makedirs(file_path)
         with open(os.path.join(file_path, AUDIO_FILE_NAME), "wb") as f:
@@ -121,117 +122,117 @@ async def upload_file(file: UploadFile, user_id: str):
         # Return the unique identifier associated with the uploaded file
         return save_process(user_id, unique_id, file.filename)
 
-    return JSONResponse(content={'error': 'Invalid file type'}, status_code=400)
+    return JSONResponse(content={"error": "Invalid file type"}, status_code=400)
 
 
 @app.post("/process-url")
 async def download_audio(data: dict, user_id: str):
     print(data)
-    '''Get audio URL'''
-    if 'content_id' not in data or 'format' not in data:
-        return JSONResponse(content={'error': 'Invalid request'}, status_code=400)
+    """Get audio URL"""
+    if "content_id" not in data or "format" not in data:
+        return JSONResponse(content={"error": "Invalid request"}, status_code=400)
 
-    content_id = data['content_id']
-    request_format = data['format']
+    content_id = data["content_id"]
+    request_format = data["format"]
 
-    if (request_format == 'youtube'):
+    if request_format == "youtube":
         save_youtube_audio(user_id, content_id)
+
 
 @app.get("/info/{user_id}")
 async def return_track_info(user_id: str):
-    '''Track Identification'''
+    """Track Identification / Genre Prediction"""
     if user_id in user_files:
         file_info = user_files[user_id]
-        file_path = file_info['process_id']
+        file_path = file_info["process_id"]
 
         # TODO: add track identification
 
-
-        return JSONResponse(content={'text': 'YAY'})
+        return JSONResponse(content={"text": "YAY"})
     else:
-        return JSONResponse(content={'error': 'User has no uploaded files'}, status_code=404)
+        return JSONResponse(
+            content={"error": "User has no uploaded files"}, status_code=404
+        )
+
 
 @app.get("/audio-file/{user_id}")
 async def return_user_file(user_id: str):
-    '''Send the file requested'''
+    """Send the file requested"""
     if user_id in user_files:
         file_info = user_files[user_id]
-        return FileResponse(path=os.path.join(PROCESS_FOLDER, file_info['process_id'], AUDIO_FILE_NAME),
-                            filename=AUDIO_FILE_NAME, media_type='application/octet-stream')
+        return FileResponse(
+            path=os.path.join(PROCESS_FOLDER, file_info["process_id"], AUDIO_FILE_NAME),
+            filename=AUDIO_FILE_NAME,
+            media_type="application/octet-stream",
+        )
     else:
-        return JSONResponse(content={'error': 'User has no uploaded files'}, status_code=404)
+        return JSONResponse(
+            content={"error": "User has no uploaded files"}, status_code=404
+        )
+
 
 @app.get("/midi-file/{user_id}")
 async def convert_audio_to_midi(user_id: str):
-    '''Send the file requested'''
+    """Audio to MIDI"""
     if user_id in user_files:
         file_info = user_files[user_id]
-        file_in = os.path.join(PROCESS_FOLDER, file_info['process_id'], AUDIO_FILE_NAME)
-        file_out = os.path.join(PROCESS_FOLDER, file_info['process_id'], MIDI_FILE_NAME)
-        y, sr = librosa.load(file_in, sr=None)
-        print("Converting...", sr)
-        midi = wave_to_midi(y, srate=int(sr)) # TODO: Improve MIDI conversion
-        print("Done converting!")
-        with open (file_out, 'wb') as f:
-            midi.writeFile(f)
-        return FileResponse(path=os.path.join(PROCESS_FOLDER, file_info['process_id'], MIDI_FILE_NAME),
-                            filename=MIDI_FILE_NAME, media_type='application/octet-stream')
+
+        file_in = os.path.join(PROCESS_FOLDER, file_info["process_id"], AUDIO_FILE_NAME)
+        out_dir = os.path.join(PROCESS_FOLDER, file_info["process_id"])
+
+        predict_and_save(
+            [file_in],
+            out_dir,
+            save_midi=True,
+            sonify_midi=False,
+            save_model_outputs=False,
+            save_notes=False,
+        )
+
+        return FileResponse(
+            path=os.path.join(out_dir, MIDI_FILE_NAME),
+            filename=MIDI_FILE_NAME,
+            media_type="application/octet-stream",
+        )
     else:
-        return JSONResponse(content={'error': 'User has no uploaded files'}, status_code=404)
+        return JSONResponse(
+            content={"error": "User has no uploaded files"}, status_code=404
+        )
+
 
 # TODO: Add more audio processing
-@app.post("/separate")
-async def separate_audio(audio_file: UploadFile = File(...)):
-    if not audio_file.filename.lower().endswith(tuple(ALLOWED_AUDIO_EXTENSIONS)):
-        raise HTTPException(status_code=400, detail="Only audio files with extensions .mp3, .wav, or .ogg are supported")
+@app.get("/separate/{user_id}")
+async def separate_audio(user_id: str):
+    """Audio to MIDI"""
+    if user_id in user_files:
+        file_info = user_files[user_id]
 
-    file_path = f"./uploads/{audio_file.filename}"
-    with open(file_path, "wb") as f:
-        shutil.copyfileobj(audio_file.file, f)
+        file_in = os.path.join(PROCESS_FOLDER, file_info["process_id"], AUDIO_FILE_NAME)
+        out_dir = os.path.join(PROCESS_FOLDER, file_info["process_id"])
 
-    output_path = './output/track_split/'
-    os.makedirs(output_path, exist_ok=True)
-    separator.separate_to_file(file_path, output_path)
+        separator.separate_to_file(file_in, out_dir)
+        shutil.make_archive("splitted_audio", "zip", os.path.join(out_dir, "audio"))
+        shutil.move("splitted_audio.zip", out_dir)
 
-    return {
-        'vocals_path': f'{output_path}audio_example/vocals.wav',
-        'accompaniment_path': f'{output_path}audio_example/accompaniment.wav'
-    }
+        return FileResponse(
+            path=os.path.join(out_dir, "splitted_audio.zip"),
+            filename=MIDI_FILE_NAME,
+            media_type="application/zip",
+        )
+    else:
+        return JSONResponse(
+            content={"error": "User has no uploaded files"}, status_code=404
+        )
 
-@app.post("/audio2midi")
-async def audio_to_midi(audio_file: UploadFile = File(...)):
-    if not audio_file.filename.lower().endswith(tuple(ALLOWED_AUDIO_EXTENSIONS)):
-        raise HTTPException(status_code=400, detail="Only audio files with extensions .mp3, .wav, or .ogg are supported")
-
-    file_path = f"./temp/{audio_file.filename}"
-    with open(file_path, "wb") as f:
-        shutil.copyfileobj(audio_file.file, f)
-
-    
-    output_path = './output/audio2midi/'
-    os.makedirs(output_path, exist_ok=True)
-    predict_and_save(
-        [file_path],
-        output_path,
-        save_midi=True,
-        sonify_midi=False,
-        save_model_outputs=False,
-        save_notes=False,
-    )
-
-    return {
-        'midi_path': f'{output_path}/{os.path.splitext(os.path.basename(file_path))[0]}_basic_pitch.mid',
-    }
 
 @app.post("/genre_predict")
 async def genre_predict():
     return {}
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     if not os.path.exists(PROCESS_FOLDER):
         os.makedirs(PROCESS_FOLDER)
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=5000,
-                log_level="info", reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, log_level="info", reload=True)
